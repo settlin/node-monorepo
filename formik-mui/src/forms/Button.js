54ffb4aa7d301f0df9e2496bf6048b65adb680f7
Sudhanshu <sudhanshu@settlin.in>
2019-03-23 22:16:48 +0530

    fix: dropzone & select

--- formik-mui/src/forms/Button.js
+++ formik-mui/src/forms/Button.js
@@ -2,13 +2,13 @@ import React from 'react';
 import clsx from 'clsx';
 import withStyles from '@material-ui/core/styles/withStyles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import green from '@material-ui/core/colors/green';
+import teal from '@material-ui/core/colors/teal';
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
 import CheckIcon from '@material-ui/icons/Check';
-import SaveIcon from '@material-ui/icons/Save';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
-const styles = () => ({
+const styles = (theme) => ({
 	root: {
 		display: 'flex',
 		alignItems: 'center',
@@ -18,28 +18,31 @@ const styles = () => ({
 		margin: 'auto',
 		position: 'relative',
 	},
+	marginRight: {
+		marginRight: theme.spacing,
+	},
 	wrapperFullWidth: {
 		margin: 'auto',
 		position: 'relative',
 		width: '100%',
 	},
 	buttonSuccess: {
-		backgroundColor: green[500],
+		backgroundColor: teal[500],
 		'&:hover': {
-			backgroundColor: green[700],
+			backgroundColor: teal[700],
 		},
 		backgroundImage: 'url(https://static.financebuddha.com/assets/images/gradient-bg.png)',
 		backgroundSize: 'contain',
 	},
 	fabProgress: {
-		color: green[500],
+		color: teal[500],
 		position: 'absolute',
 		top: -6,
 		left: -6,
 		zIndex: 1,
 	},
 	buttonProgress: {
-		color: green[500],
+		color: teal[500],
 		position: 'absolute',
 		top: '50%',
 		left: '50%',
@@ -50,25 +53,29 @@ const styles = () => ({
 
 class CircularIntegration extends React.Component {
 	render() {
-  	const {classes, fab, processing, success, color = 'primary', variant = 'contained', children = 'Submit', fullWidth, ...rest} = this.props;
+  	const {classes, cs = {}, fab, processing, success, label = '', color = 'primary', variant = 'contained', children = 'Submit', Icon, fullWidth, ...rest} = this.props;
   	const buttonClassname = clsx({
   		[classes.buttonSuccess]: success,
-  	});
+		});
+
+		const IconComp = success ? CheckIcon : Icon;
 
   	return (
 			<div className={classes.root} style={{width: fullWidth ? '100%' : 'auto'}}>
   			{fab
   				? (
   					<div className={classes.wrapper}>
-  						<Fab color={color} className={buttonClassname} {...rest}>
-  							{success ? <CheckIcon/> : <SaveIcon/>}
+  						<Fab color={color} classes={{root: clsx(buttonClassname, cs.button)}} {...rest}>
+  							{success ? <CheckIcon/> : <CloudUploadIcon/>}
   						</Fab>
   						{processing && <CircularProgress size={68} className={classes.fabProgress}/>}
   					</div>
   				)
   				: (
   					<div className={fullWidth ? classes.wrapperFullWidth : classes.wrapper}>
-  						<Button fullWidth={fullWidth} variant={variant} color={color} className={buttonClassname} disabled={processing} {...rest}>
+  						<Button fullWidth={fullWidth} variant={variant} color={color} classes={{root: clsx(buttonClassname, cs.button)}} disabled={processing} {...rest}>
+								{IconComp && <IconComp classes={{root: clsx(classes.marginRight, cs.icon)}}/>}
+								{label}
 								{children}
   						</Button>
   						{processing && <CircularProgress size={24} className={classes.buttonProgress}/>}
@@ -79,6 +86,10 @@ class CircularIntegration extends React.Component {
   	);
 	}
 }
-CircularIntegration.displayName = 'FButton';
 
-export default withStyles(styles)(CircularIntegration);
+const Button1 = withStyles(styles)(CircularIntegration);
+const Button2 = ({classes, ...props}) => <Button1 cs={classes} {...props}/>;
+
+Button2.displayName = 'FButton';
+
+export default Button2;
