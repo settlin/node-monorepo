34ba4359dfb4e4563ee10973c261f1bf144c02be
akshay <akshaya.h@settlin.in>
2021-10-12 16:23:40 +0530

    fix: slider improvements

--- react-hook-form-mui/src/forms/Slider.js
+++ react-hook-form-mui/src/forms/Slider.js
@@ -1,12 +1,11 @@
 /* eslint-disable react/no-multi-comp */
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Typography, Slider, Grid, Tooltip, FormControl, FormLabel, FormHelperText} from '@material-ui/core';
-import currencify from '../utils/currencify';
+import {Slider, Tooltip, FormControl, FormLabel, FormHelperText} from '@material-ui/core';
 
 function ValueLabelComponent({children, value}) {
 	return (
-		<Tooltip arrow placement='top' title={value > 100000 ? currencify(value, {numberWithCommas: true}) : value}>
+		<Tooltip arrow placement='top' title={value}>
 			{children}
 		</Tooltip>
 	);
@@ -18,34 +17,12 @@ ValueLabelComponent.propTypes = {
 };
 
 // eslint-disable-next-line react/prop-types
-function SliderButton({totalStep, SliderProps = {}, FormControlProps = {}, helperText, label, showMarks = false, displayValue = 'auto', name, valueLabelFormat, minValue, value, setValue, maxValue, ...props}) {
-	// console.log('props are', props);
+function SliderButton({totalStep, SliderProps, FormControlProps = {}, helperText, label, showMarks = false, displayValue = 'auto', name, valueLabelFormat, minValue, value, setValue, maxValue, ...props}) {
 	const handleChange = (event, newVal) => {
 		setValue(name, newVal);
 	};
 
-	// const changeVal = (v) => {
-	// 	return v + 5000;
-	// };
-
 	return (
-		// <Grid style={{marginTop: '0.6rem'}}>
-		// 	<Typography>
-		// 		{label}
-		// 	</Typography>
-		// 	<Slider
-		// 		className={SliderProps}
-		// 		marks={showMarks}
-		// 		max={maxValue}
-		// 		min={minValue}
-		// 		step={totalStep}
-		// 		{...props}
-		// 		ValueLabelComponent={ValueLabelComponent}
-		// 		onChange={handleChange}
-		// 		value={value}
-		// 		valueLabelDisplay={displayValue}
-		// 	/>
-		// </Grid>
 		<FormControl {...FormControlProps}>
 			<FormLabel>
 				{label}
@@ -60,7 +37,6 @@ function SliderButton({totalStep, SliderProps = {}, FormControlProps = {}, helpe
 				ValueLabelComponent={ValueLabelComponent}
 				onChange={handleChange}
 				value={value}
-				// value={()=>changeVal(value)}
 				valueLabelDisplay={displayValue}
 				valueLabelFormat={(v)=>valueLabelFormat(v)}
 			/>
@@ -79,7 +55,7 @@ SliderButton.propTypes = {
 	name: PropTypes.string,
 	setValue: PropTypes.func,
 	showMarks: PropTypes.bool,
-	SliderProps: PropTypes.object,
+	SliderProps: PropTypes.string,
 	totalStep: PropTypes.number,
 };
 
