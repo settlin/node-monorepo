54661c3476add5d7d01f5027d073cc70dbd081eb
Aniket Teltumbade <aniket.teltumbade@settlin.in>
2022-04-22 16:40:51 +0530

    upgrade(formik-ui): material ui 4 to 5 migration

--- formik-mui/src/forms/FormControlLabel.js
+++ formik-mui/src/forms/FormControlLabel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { useFormControl } from '@mui/material/FormControl';
+import {useFormControl} from '@mui/material/FormControl';
 import withStyles from '@mui/styles/withStyles';
 import Typography from '@mui/material/Typography';
 
