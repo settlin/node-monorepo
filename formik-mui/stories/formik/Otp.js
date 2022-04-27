bb26f0822b7951d248c7ed218f97549393651ec2
Aniket Teltumbade <aniket.teltumbade@settlin.in>
2022-04-27 17:46:28 +0530

    fix: formatting issue resolved

--- formik-mui/stories/formik/Otp.js
+++ formik-mui/stories/formik/Otp.js
@@ -20,7 +20,7 @@ class DemoForm extends PureComponent {
 		const initialValues = {mobile: '80808080'};
 
 		return (
-            <Grid container item spacing={1} style={{padding: '2rem'}}>
+			<Grid container item spacing={1} style={{padding: '2rem'}}>
 				<Formik initialValues={initialValues} enableReinitialize={true} isInitialValid={false} onSubmit={this.hSubmit}>
 					{({isSubmitting, values, errors, isValid}) => <Form autoComplete='off' style={{width: '100%'}}>
 						<Grid item xs={12}>
@@ -41,7 +41,7 @@ class DemoForm extends PureComponent {
 					</Form>}
 				</Formik>
 			</Grid>
-        );
+		);
 	}
 }
 
