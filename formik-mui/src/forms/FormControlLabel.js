5c789853e677f5bb311273cae80bd4cca7818e08
Aniket Teltumbade <aniket.teltumbade@settlin.in>
2022-07-21 13:45:05 +0530

    fix: git resolved

--- formik-mui/src/forms/FormControlLabel.js
+++ formik-mui/src/forms/FormControlLabel.js
@@ -6,7 +6,7 @@ import withStyles from "@mui/styles/withStyles";
 import Typography from "@mui/material/Typography";
 
 function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export const styles = theme => ({
@@ -53,145 +53,130 @@ export const styles = theme => ({
 		},
 	},
 });
-
 /**
  * Drop in replacement of the `Radio`, `Switch` and `Checkbox` component.
  * Use this component if you want to display an extra label.
  */
-const FormControlLabel = React.forwardRef(function FormControlLabel(
-  props,
-  ref
-) {
-  const {
-    checked, // eslint-disable-line no-unused-vars
-    classes,
-    className: classNameProp,
-    control,
-    disabled: disabledProp,
-    inputRef, // eslint-disable-line no-unused-vars
-    label,
-    labelPlacement = "end",
-    name, // eslint-disable-line no-unused-vars
-    offLabel,
-    onChange, // eslint-disable-line no-unused-vars
-    value, // eslint-disable-line no-unused-vars
-    ...other
-  } = props;
-  const muiFormControl = useFormControl();
+const FormControlLabel = React.forwardRef(function FormControlLabel(props, ref) {
+	const {
+		checked, // eslint-disable-line no-unused-vars
+		classes,
+		className: classNameProp,
+		control,
+		disabled: disabledProp,
+		inputRef, // eslint-disable-line no-unused-vars
+		label,
+		labelPlacement = 'end',
+		name, // eslint-disable-line no-unused-vars
+		offLabel,
+		onChange, // eslint-disable-line no-unused-vars
+		value, // eslint-disable-line no-unused-vars
+		...other
+	} = props;
+	const muiFormControl = useFormControl();
 
-  let disabled = disabledProp;
-  if (
-    typeof disabled === "undefined" &&
-    typeof control.props.disabled !== "undefined"
-  ) {
-    disabled = control.props.disabled;
-  }
-  if (typeof disabled === "undefined" && muiFormControl) {
-    disabled = muiFormControl.disabled;
-  }
+	let disabled = disabledProp;
+	if (typeof disabled === 'undefined' && typeof control.props.disabled !== 'undefined') {
+		disabled = control.props.disabled;
+	}
+	if (typeof disabled === 'undefined' && muiFormControl) {
+		disabled = muiFormControl.disabled;
+	}
 
-  const controlProps = {
-    disabled,
-  };
-  ["checked", "name", "onChange", "value", "inputRef"].forEach((key) => {
-    if (
-      typeof control.props[key] === "undefined" &&
-      typeof props[key] !== "undefined"
-    ) {
-      controlProps[key] = props[key];
-    }
-  });
+	const controlProps = {
+		disabled,
+	};
+	['checked', 'name', 'onChange', 'value', 'inputRef'].forEach(key => {
+		if (typeof control.props[key] === 'undefined' && typeof props[key] !== 'undefined') {
+			controlProps[key] = props[key];
+		}
+	});
 
-  return (
-    <label
-      className={clsx(
-        classes.root,
-        {
-          [classes[`labelPlacement${capitalize(labelPlacement)}`]]:
-            labelPlacement !== "end",
-          [classes.disabled]: disabled,
-          [classes.marginLeft0]: offLabel,
-        },
-        classNameProp
-      )}
-      ref={ref}
-      {...other}
-    >
-      {offLabel && (
-        <Typography
-          component="span"
-          className={clsx(classes.label, { [classes.disabled]: disabled })}
-        >
-          {offLabel}
-        </Typography>
-      )}
-      {React.cloneElement(control, controlProps)}
-      <Typography
-        component="span"
-        className={clsx(classes.label, { [classes.disabled]: disabled })}
-      >
-        {label}
-      </Typography>
-    </label>
-  );
+	return (
+		<label
+			className={clsx(
+				classes.root,
+				{
+					[classes[`labelPlacement${capitalize(labelPlacement)}`]]: labelPlacement !== 'end',
+					[classes.disabled]: disabled,
+					[classes.marginLeft0]: offLabel,
+				},
+				classNameProp,
+			)}
+			ref={ref}
+			{...other}
+		>
+			{offLabel && <Typography
+				component='span'
+				className={clsx(classes.label, {[classes.disabled]: disabled})}
+			>
+				{offLabel}
+			</Typography>}
+			{React.cloneElement(control, controlProps)}
+			<Typography
+				component='span'
+				className={clsx(classes.label, {[classes.disabled]: disabled})}
+			>
+				{label}
+			</Typography>
+		</label>
+	);
 });
 
 FormControlLabel.propTypes = {
-  /**
+	/**
    * If `true`, the component appears selected.
    */
-  checked: PropTypes.bool,
-  /**
+	checked: PropTypes.bool,
+	/**
    * Override or extend the styles applied to the component.
    * See [CSS API](#css) below for more details.
    */
-  classes: PropTypes.object.isRequired,
-  /**
+	classes: PropTypes.object.isRequired,
+	/**
    * @ignore
    */
-  className: PropTypes.string,
-  /**
+	className: PropTypes.string,
+	/**
    * A control element. For instance, it can be be a `Radio`, a `Switch` or a `Checkbox`.
    */
-  control: PropTypes.element,
-  /**
+	control: PropTypes.element,
+	/**
    * If `true`, the control will be disabled.
    */
-  disabled: PropTypes.bool,
-  /**
+	disabled: PropTypes.bool,
+	/**
    * This prop can be used to pass a ref callback to the `input` element.
    */
-  inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-  /**
+	inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+	/**
    * The text to be used in an enclosing label element.
    */
-  label: PropTypes.node,
-  /**
+	label: PropTypes.node,
+	/**
    * The position of the label.
    */
-  labelPlacement: PropTypes.oneOf(["end", "start", "top", "bottom"]),
-  /**
+	labelPlacement: PropTypes.oneOf(['end', 'start', 'top', 'bottom']),
+	/**
    * @ignore
    */
-  muiFormControl: PropTypes.object,
-  /*
+	muiFormControl: PropTypes.object,
+	/*
    * @ignore
    */
-  name: PropTypes.string,
-  /**
+	name: PropTypes.string,
+	/**
    * Callback fired when the state is changed.
    *
    * @param {object} event The event source of the callback.
    * You can pull out the new value by accessing `event.target.checked`.
    * @param {boolean} checked The `checked` value of the switch
    */
-  onChange: PropTypes.func,
-  /**
+	onChange: PropTypes.func,
+	/**
    * The value of the component.
    */
-  value: PropTypes.any,
+	value: PropTypes.any,
 };
 
-export default withStyles(styles, { name: 'MuiFormControlLabel' })(
-  FormControlLabel
-);
+export default withStyles(styles, {name: 'MuiFormControlLabel'})(FormControlLabel);
