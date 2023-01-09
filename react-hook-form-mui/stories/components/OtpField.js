a87992b0557eb866836862efb9a7b34509dbbf87
Sudhanshu <sudhanshu@settlin.in>
2023-01-09 18:58:24 +0530

    fix: fixes

--- react-hook-form-mui/stories/components/OtpField.js
+++ react-hook-form-mui/stories/components/OtpField.js
@@ -3,7 +3,7 @@ import FButton from '../../src/forms/Button';
 import InputAdornment from '@mui/material/InputAdornment';
 import Input from '../../src/Input';
 import Clear from '@mui/icons-material/Clear';
-import TextField from '../../src/formik/TextField';
+import TextField from '../../src/react-hook-form/TextField';
 import Select from './FilterField';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
@@ -19,20 +19,20 @@ const styles = () => ({
 });
 
 class ResendButton extends React.PureComponent {
-	state = { remainingSecs: 30 }
+	state = {remainingSecs: 30}
 	tick() {
-		let { remainingSecs } = this.state;
+		let {remainingSecs} = this.state;
 		if (remainingSecs === 1) clearInterval(this.timer);
-		this.setState({ remainingSecs: remainingSecs - 1 });
+		this.setState({remainingSecs: remainingSecs - 1});
 	}
 	checkTick() {
-		const { tick } = this.props;
+		const {tick} = this.props;
 		if (tick === 'start') {
-			this.setState({ remainingSecs: 30 });
+			this.setState({remainingSecs: 30});
 			this.timer = setInterval(this.tick, 1000);
 		}
 		if (tick === 'clear') {
-			this.setState({ remainingSecs: 0 });
+			this.setState({remainingSecs: 0});
 			clearInterval(this.timer);
 		}
 	}
@@ -43,17 +43,19 @@ class ResendButton extends React.PureComponent {
 		if (pp.tick !== this.props.tick) this.checkTick();
 	}
 	render() {
-		const { onClick, processing, startCounterAgain } = this.props;
-		const { remainingSecs } = this.state;
+		const {onClick, processing, startCounterAgain} = this.props;
+		const {remainingSecs} = this.state;
 		remainingSecs === 0 && startCounterAgain();
-		return <FButton size='small' variant='text' disabled={Boolean(remainingSecs)} onClick={onClick} processing={processing}>
+		return (
+<FButton size='small' variant='text' disabled={Boolean(remainingSecs)} onClick={onClick} processing={processing}>
 			Resend {remainingSecs > 0 ? `in ${remainingSecs}s` : ''}
-		</FButton>;
+		</FButton>
+);
 	}
 }
 
 class UserPhoneOtp extends React.PureComponent {
-	state = { otp: '' }
+	state = {otp: ''}
 	constructor(props) {
 		super(props);
 		this.hSend = this.hSend.bind(this);
@@ -63,86 +65,86 @@ class UserPhoneOtp extends React.PureComponent {
 		this.hEnterOnMobile = this.hEnterOnMobile.bind(this);
 		this.hEnterOnOtp = this.hEnterOnOtp.bind(this);
 	}
-	hSend({ resend } = {}) {
-		let { otpPurpose = 'verification', onOtpSend, field: { name, value } = {}, form: { errors = {}, setFieldError } = {} } = this.props;
+	hSend({resend} = {}) {
+		let {otpPurpose = 'verification', onOtpSend, field: {name, value} = {}, form: {errors = {}, setFieldError} = {}} = this.props;
 		if (!value || errors[name]) return;
 
 		const mobile = value.toString();
-		this.setState({ isSending: true });
-		Meteor.call('otp.create', { phone: mobile, purpose: otpPurpose, resend }, (err) => {
-			if (onOtpSend) onOtpSend({ error: err, mobile });
+		this.setState({isSending: true});
+		Meteor.call('otp.create', {phone: mobile, purpose: otpPurpose, resend}, (err) => {
+			if (onOtpSend) onOtpSend({error: err, mobile});
 			if (err) {
 				if (setFieldError) setFieldError(name, err.reason);
-				this.setState({ isSending: false });
+				this.setState({isSending: false});
 				return;
 			}
-			this.setState({ sent: true, tick: 'start', isSending: false });
+			this.setState({sent: true, tick: 'start', isSending: false});
 		});
 	}
 	hVerify() {
-		const { otpPurpose = 'verification', onOtpSubmitCallback, user, expectedUserId, field: { value } = {}, form } = this.props;
-		const { otp } = this.state;
+		const {otpPurpose = 'verification', onOtpSubmitCallback, user, expectedUserId, field: {value} = {}, form} = this.props;
+		const {otp} = this.state;
 		const mobile = value.toString();
 
 		const otpError = this.otpError(otp);
 		if (otpError) {
-			this.setState({ otpError });
+			this.setState({otpError});
 			return;
 		}
-		this.setState({ isVerifying: true });
+		this.setState({isVerifying: true});
 		let cb = (obj) => {
 			if (obj.verified && form) form.setFieldValue('userId', Meteor.userId());
-			this.setState({ isVerifying: false, ...obj });
+			this.setState({isVerifying: false, ...obj});
 			form.setFieldError('otp', obj.otpError);
 		};
 		if ((user || {})._id) {
-			Meteor.call('user.verify', { expectedUserId, phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : '' }, (err) => {
+			Meteor.call('user.verify', {expectedUserId, phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : ''}, (err) => {
 				if (err) {
 					switch (err.error) {
 						case 'phone-already-associated':
-							cb({ phoneAssociatedData: err.details, tick: 'clear' });
+							cb({phoneAssociatedData: err.details, tick: 'clear'});
 							break;
 						case 'incorrect-otp':
-							cb({ otp: '', otpError: err.reason });
+							cb({otp: '', otpError: err.reason});
 							break;
 						default:
-							Log.fatal(err.reason, err, { expectedUserId, phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : '' });
-							cb({ otpError: err.reason, tick: 'clear' });
+							Log.fatal(err.reason, err, {expectedUserId, phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : ''});
+							cb({otpError: err.reason, tick: 'clear'});
 							break;
 					}
 					return;
 				}
-				if (onOtpSubmitCallback) onOtpSubmitCallback({ cb: cb({ verified: true, tick: 'clear' }) });
-				else cb({ verified: true, tick: 'clear' });
+				if (onOtpSubmitCallback) onOtpSubmitCallback({cb: cb({verified: true, tick: 'clear'})});
+				else cb({verified: true, tick: 'clear'});
 			});
 		}
 		else {
-			let options = { phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : '', expectedUserId };
+			let options = {phone: mobile, otp, purpose: otpPurpose !== 'login' ? otpPurpose : '', expectedUserId};
 			Meteor.loginWithPhone(options, (err) => {
 				if (err) {
 					switch (err.error) {
 						case 'incorrect-otp':
-							cb({ otp: '', otpError: err.reason });
+							cb({otp: '', otpError: err.reason});
 							break;
 						case 'unexpected-user':
-							cb({ phoneAssociatedData: err.details, tick: 'clear' });
+							cb({phoneAssociatedData: err.details, tick: 'clear'});
 							return;
 						default:
 							Log.fatal('loginWithPhone 1: ' + err.reason, err, options);
-							cb({ otpError: err.reason, tick: 'clear' });
+							cb({otpError: err.reason, tick: 'clear'});
 							break;
 					}
 				}
 				else {
 					Meteor.call('logs.addLogin');
-					if (onOtpSubmitCallback) onOtpSubmitCallback({ cb: cb({ verified: true, tick: 'clear' }) });
-					else cb({ verified: true, tick: 'clear' });
+					if (onOtpSubmitCallback) onOtpSubmitCallback({cb: cb({verified: true, tick: 'clear'})});
+					else cb({verified: true, tick: 'clear'});
 				}
 			});
 		}
 	}
 	hCancelOtp() {
-		this.setState({ tick: 'clear', otp: '', otpError: false, sent: false });
+		this.setState({tick: 'clear', otp: '', otpError: false, sent: false});
 		if (this.props.onOtpCancel) this.props.onOtpCancel();
 	}
 	otpError(otp) {
@@ -150,7 +152,7 @@ class UserPhoneOtp extends React.PureComponent {
 	}
 	hSetOtp(e) {
 		const otp = e.currentTarget.value;
-		this.setState({ otpError: this.otpError(otp), otp });
+		this.setState({otpError: this.otpError(otp), otp});
 	}
 	hEnterOnOtp(e) {
 		if (e.key === 'Enter') {
@@ -164,59 +166,69 @@ class UserPhoneOtp extends React.PureComponent {
 			this.hSend();
 		}
 	}
-	phones({ verified = false } = {}) {
-		const { user } = this.props;
+	phones({verified = false} = {}) {
+		const {user} = this.props;
 		if (!((user || {}).phones || []).length) return [];
-		return user.phones.filter(p => (p.number || '').startsWith('+91') && (verified ? p.verified : true)).map(p => ({ value: p.number.substr(3), label: p.number }));
+		return user.phones.filter(p => (p.number || '').startsWith('+91') && (verified ? p.verified : true)).map(p => ({value: p.number.substr(3), label: p.number}));
 	}
 	checkVerified(phoneNumber) {
-		const { user } = this.props;
-		!user && this.setState({ verified: false });
+		const {user} = this.props;
+		!user && this.setState({verified: false});
 		const parent = this;
 		const phones = user && user.phones;
 		phoneNumber && phones && phones.map(e => {
-			e.number && e.number.replace(e.code || '+91', '') === (phoneNumber && phoneNumber.toString().replace('+91', '')) && e.verified && e.verified === true && parent.setState({ verified: true });
+			e.number && e.number.replace(e.code || '+91', '') === (phoneNumber && phoneNumber.toString().replace('+91', '')) && e.verified && e.verified === true && parent.setState({verified: true});
 		});
 	}
 	render() {
-		let { onOtpSubmitCallback, user, dispatch, classes, ...props } = this.props;  // eslint-disable-line no-unused-vars
-		const { sent, otp, otpError, isSending, isVerifying, verified, tick } = this.state;
-		const { hSend, hVerify, hSetOtp, hEnterOnMobile, hEnterOnOtp, hCancelOtp } = this;
+		let {onOtpSubmitCallback, user, dispatch, classes, ...props} = this.props;  // eslint-disable-line no-unused-vars
+		const {sent, otp, otpError, isSending, isVerifying, verified, tick} = this.state;
+		const {hSend, hVerify, hSetOtp, hEnterOnMobile, hEnterOnOtp, hCancelOtp} = this;
 		const phones = this.phones();
 		// const verifiedPhones = this.phones({verified: true});
 
-		const { field, form, ...pureProps } = props; // eslint-disable-line no-unused-vars
+		const {field, form, ...pureProps} = props; // eslint-disable-line no-unused-vars
 		const value = props.value || (props.field || {}).value;
 		// this.checkVerified(value);
 		const mobile = (value || '').toString();
 		return (
-            <Grid container justifyContent='center'>
-				{/* {phoneAssociatedData && <Dialog {...other}>
+			<Grid container justifyContent='center'>
+	{/* {phoneAssociatedData && <Dialog {...other}>
 				  <DialogTitle>Phone number already associated with another user.</DialogTitle>
 					<EmailOrPhoneAssociatedModal data={phoneAssociatedData} resetData={() => this.setState({phoneAssociatedData: null})} onOtpSubmitCallback={onOtpSubmitCallback}/>
 				</Dialog>} */}
-				<Grid item xs={12}>
+	<Grid item xs={12}>
 					{verified
 						? (
-							<React.Fragment>
+							<>
 								<TextField {...props} type='number' inputProps={{ readOnly: true }} InputProps={{
 									endAdornment: <InputAdornment><Icon classes={{ root: classes.doneAll }} aria-label='Verified'><DoneAll /></Icon></InputAdornment>,
 								}} />
 								{(user || {})._id && <Input value={user._id} name='userId' label='' type='hidden' />}
-							</React.Fragment>
+							</>
 						)
 						: sent
 							? (
-								<TextField name='otp' label='OTP' type='number' value={otp} error={Boolean(otpError)} helperText={otpError} onChange={hSetOtp} onKeyDown={hEnterOnOtp} InputProps={{
+								<TextField
+name='otp'
+label='OTP'
+type='number'
+value={otp}
+error={Boolean(otpError)}
+helperText={otpError}
+onChange={hSetOtp}
+onKeyDown={hEnterOnOtp}
+InputProps={{
 									endAdornment: <InputAdornment><IconButton aria-label='Cancel Otp' onClick={hCancelOtp} size="large"><Clear /></IconButton></InputAdornment>,
-								}} />
+								}}/>
 							)
 							: phones.length
-								? <Select {...props} defaultValue={phones[0].value} options={phones} />
-								: <TextField {...props} type='number' onKeyDown={hEnterOnMobile} />
+								? <Select {...props} defaultValue={phones[0].value} options={phones}/>
+								: <TextField {...props} onKeyDown={hEnterOnMobile} type='number'/>
 					}
 				</Grid>
-				{!verified && <Grid container item xs={12} justifyContent={sent ? 'space-between' : 'flex-end'}>
+	{!verified && (
+<Grid container item xs={12} justifyContent={sent ? 'space-between' : 'flex-end'}>
 					{sent
 						? (
 							<React.Fragment>
@@ -226,9 +238,10 @@ class UserPhoneOtp extends React.PureComponent {
 						)
 						: <FButton size='small' variant='outlined' color='primary' onClick={hSend} processing={isSending} success={sent}>Send OTP</FButton>
 					}
-				</Grid>}
-			</Grid>
-        );
+				</Grid>
+)}
+</Grid>
+		);
 	}
 }
 
